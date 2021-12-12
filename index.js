4c4eed239ca38b3d0df24e8e113bf9b1650c99d9
azder <azhder@gmail.com>
2021-12-12 13:13:10 +0100

    Add display of version

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
-
+import {createRequire} from 'node:module';
 import create$ from './src/create$.fn.js';
 import bail$ from './src/helpers/bail$.fn.js';
+import CRAYON from './src/helpers/crayon.const.js';
+
+
+const {bg: {black: BG}, fg: {white: FG, blue: VER}, reset: RST} = CRAYON;
+
+
+const {version} = createRequire(import.meta.url)('./data.json') ?? {};
+// eslint-disable-next-line no-console
+console.log(`${FG}${BG}version: ${VER}${version}${RST}`);
 
 
 create$().catch(bail$);
